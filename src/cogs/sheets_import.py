e56925831035a5364a43b370d1e934180ff4be00
Jessica Eisler <jne1747@g.rit.edu>
2022-02-27 07:14:28 -0500

    Update Import

--- src/cogs/sheets_import.py
+++ src/cogs/sheets_import.py
@@ -24,11 +24,14 @@ class sheets_import_cog(commands.Cog):
         # pulling the column of the google sheet
         string_id_array = sheets.pull_column()
 
+        array_ids = ""
         # looping through the DiscordIds and printing them to the chat one by one
         for x in string_id_array:
             x = x.value.strip()
             name, discrim = x.split("#")
-            await ctx.respond(discord.utils.get(self.bot.get_all_members(), name=name, discriminator=discrim))
+            array_ids += str(discord.utils.get(self.bot.get_all_members(), name=name, discriminator=discrim).id)
+            # await ctx.respond(discord.utils.get(self.bot.get_all_members(), name=name, discriminator=discrim))
+        await ctx.respond(array_ids)
 
 
 def setup(bot: commands.Bot):
---
+++ src/config.py
@@ -0,0 +1,16 @@
+#
+# RENAME THIS FILE TO CONFIG.PY!
+#
+
+# for help please check the GitHub wiki
+
+# discord api
+DISCORD_TOKEN = "OTQ3MTczOTE3MDcwNjEwNTAz.Yhpacw.EZvg69dEsAwRYoowfqvEkvQQlrI"
+GUILD_IDS = [947170492769521675]
+
+# sql address
+SQL_ADDRESS = "postgresql://lgeqiruo:6QSIeY6bxqd8KXajOJZcQ9jf2PhXgjqE@jelani.db.elephantsql.com/lgeqiruo"
+
+# google sheet settings
+COLUMN_CHARACTER = "C"
+SHEET_DOCUMENT_ID = "1I5hDFF9mPaqbqEHPyf2mzGtL4tZAp7ToNOkUcusBHkQ"
